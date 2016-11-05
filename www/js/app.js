9295d100e2427acdef9ebd4cd3267c686fc5b058
André N. Darcie <andrendarcie@gmail.com>
2016-11-05 16:35:45 -0200

    Processing of data received by API

--- www/js/app.js
+++ www/js/app.js
@@ -6,41 +6,64 @@
 angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .factory('Weather', function($q, $http) {
+    var deferred = $q.defer();
+
+    function getCurrentWeather(lat, lng) {
+      var api_key = "";
+      var url = 'https://api.forecast.io/forecast/' + api_key + '/' + lat + ',' + lng + '?callback=JSON_CALLBACK';
+
+      $http.jsonp(url)
+        .success(deferred.resolve)
+        .error(deferred.reject);
+
+      return deferred.promise;
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-.factory('Weather', function($q, $http) {
-  var deferred = $q.defer();
 
-  function getCurrentWeather(lat, lng) {
-    var api_key = "your key api";
-    var url = 'https://api.forecast.io/forecast/' + api_key + '/' + lat +',' + lng + '?callback=JSON_CALLBACK';
+    return {
+      getCurrentWeather: getCurrentWeather
+    };
+  })
 
-    $http.jsonp(url)
-      .success(deferred.resolve)
-      .error(deferred.reject);
+.controller('WeatherCtrl', function($scope, $cordovaGeolocation, Weather) {
+  $scope.loading = true;
 
-    return deferred.promise;
-  }
-
-  return {
-    getCurrentWeather: getCurrentWeather
+  $scope.toCelsius = function(temperature) {
+    return ((temperature - 32) / 1.8).toFixed(1);
   };
-})
 
-.controller('WeatherCtrl', function($scope, Weather) {
+  $cordovaGeolocation
+    .getCurrentPosition({
+      timeout: 10000,
+      enableHighAccuracy: false
+    })
+    .then(function(position) {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
 
+      Weather.getCurrentWeather(lat, long).then(function(data) {
+        $scope.weatherInfo = data;
+        $scope.loading = false;
+      }, function(error) {
+        console.log("Error: " + error);
+      });
+    }, function(err) {
+      //TODO Display error message
+    });
 });
